b7c5b6163de846ff43eda336b0ebf2a4c368fb6e
Karam Koujan <kkoujan@e3r6p6.1337.ma>
2024-11-21 15:37:54 +0100

    first commit

---
+++ get_next_line.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 12:24:48 by kkoujan           #+#    #+#             */
+/*   Updated: 2024/11/21 15:31:59 by kkoujan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*read_line(int fd, char *str)
+{
+	char	buff[BUFFER_SIZE];
+	ssize_t	read_size;
+	char	*res;
+
+	read_size = read(fd, buff, BUFFER_SIZE - 1);
+	if (!read_size)
+		return (free(str), NULL);
+	buff[read_size] = '\0';
+	res = ft_strjoin(str, buff);
+	free(str);
+	if (!res)
+		return (free(res), free(str), NULL);
+	return (res);
+}
+
+char	*remove_overflow(char	*str)
+{
+	int		i;
+	char	*res;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
+	res = malloc((i + 1) * sizeof(char));
+	if (!res)
+		return (free(str), NULL);
+	ft_strlcpy(res, str, i + 1);
+	return (res);
+}
+
+char	*start_next_line(char	*str)
+{
+	int		i;
+	char	*res;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
+	res = malloc((ft_strlen(str) - i + 1) * sizeof(char));
+	if (!res)
+		return (free(str), NULL);
+	ft_strlcpy(res, str + i, i - ft_strlen(str) + 1);
+	return (res);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*str;
+	char		*res;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (NULL);
+	if (str == NULL)
+		str = ft_strdup("");
+	while (str != NULL && !ft_strchr(str, '\n'))
+	{
+		str = read_line(fd, str);
+	}
+	if (!str)
+		return (NULL);
+	res = remove_overflow(str);
+	if (!res)
+		return (free(res), NULL);
+	str = start_next_line(str);
+	if (!str)
+		return (free(str), NULL);
+	return (res);
+}
