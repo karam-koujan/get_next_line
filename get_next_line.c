2ee44c6a2b9057b20011f187d03ee32a91bb2baf
Karam Koujan <kkoujan@e3r3p11.1337.ma>
2024-11-22 17:13:36 +0100

    fix: handle memory leaks and use after freee

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,11 @@
 /*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 12:24:48 by kkoujan           #+#    #+#             */
-/*   Updated: 2024/11/21 21:42:00 by kkoujan          ###   ########.fr       */
+/*   Updated: 2024/11/22 17:12:21 by kkoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
 size_t	ft_strlen(const char *s)
 {
@@ -27,21 +26,19 @@ size_t	ft_strlen(const char *s)
 
 ssize_t	read_line(int fd, char **str)
 {
-	char	buff[BUFFER_SIZE];
+	char	buff[BUFFER_SIZE + 1];
 	ssize_t	read_size;
-	char	*res;
-	
+	char	*tmp;
+
 	read_size = read(fd, buff, BUFFER_SIZE);
 	if (read_size <= 0)
 		return (read_size);
-	res = malloc((read_size + 1) * sizeof(char));
-	if (!res)
-		return (free(*str), 0);
-	ft_strlcpy(res, buff, read_size + 1);
+	buff[read_size] = '\0';
+	tmp = ft_strjoin(*str, buff);
+	if (!tmp)
+		return (free(*str), *str = NULL, -1);
 	free(*str);
-	*str = ft_strjoin(*str, res);
-	if (!str)
-		return (0);
+	*str = tmp;
 	return (read_size);
 }
 
@@ -53,6 +50,8 @@ char	*remove_overflow(char	*str)
 	i = 0;
 	while (str[i] && str[i] != '\n')
 		i++;
+	if (!str[i])
+		return (ft_strdup(str));
 	if (str[i] == '\n')
 		i++;
 	res = malloc((i + 1) * sizeof(char));
@@ -70,12 +69,14 @@ char	*start_next_line(char	*str)
 	i = 0;
 	while (str[i] && str[i] != '\n')
 		i++;
+	if (!str[i])
+		return (free(str), NULL);
 	if (str[i] == '\n')
 		i++;
 	res = malloc((ft_strlen(str) - i + 1) * sizeof(char));
 	if (!res)
 		return (free(str), NULL);
-	ft_strlcpy(res, str + i, i - ft_strlen(str) + 1);
+	res = ft_strdup(str + i);
 	free(str);
 	return (res);
 }
@@ -87,8 +88,8 @@ char	*get_next_line(int fd)
 	ssize_t		read_size;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE >= 10000000  || read(fd, 0, 0) < 0)
-		return (NULL);
-	if (str == NULL)
+		return (free(str), str = NULL, NULL);
+	if (!str)
 		str = ft_strdup("");
 	while (!ft_strchr(str, '\n'))
 	{
@@ -96,6 +97,8 @@ char	*get_next_line(int fd)
 		if (read_size <= 0)
 			break ;
 	}
+	if (!str || !*str)
+		return (free(str), str = NULL, NULL);	
 	res = remove_overflow(str);
 	str = start_next_line(str);
 	return (res);
